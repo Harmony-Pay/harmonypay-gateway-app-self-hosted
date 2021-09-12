e7257b754861a20fa0cfea85518843a08443fa5d
Sekmet <sekmet@open4g.com>
2021-09-12 06:06:05 -0300

    address checking fix

--- payments-monitor/index.js
+++ payments-monitor/index.js
@@ -314,7 +314,7 @@ async function insertSettlementQuery(order_id, order_info, transaction_id) {
 const getDonationDetails = async(transaction) => {
     
     let currency_wallet = await getWalletQuery('ONE');
-    if (currency_wallet.address === null) return false;
+    if (currency_wallet === null || currency_wallet.address === null) return false;
 
     convertOneToEthAddress(currency_wallet.address).then(async wallet_address => {
 
