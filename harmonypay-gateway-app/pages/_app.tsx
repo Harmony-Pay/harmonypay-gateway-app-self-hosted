6f49f130cb06a82e55cafc3cb27acf15802dd0d9
Mauricio Z. Filho <contact@thinkincoin.com>
2023-06-25 20:33:26 -0300

    Update _app.tsx

--- harmonypay-gateway-app/pages/_app.tsx
+++ harmonypay-gateway-app/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { Provider } from "next-auth/react"
+import { Provider } from "next-auth/client"
 import type { AppProps } from "next/app"
 import "./styles.css"
 import "./custom.css"
@@ -6,13 +6,10 @@ import "./custom.css"
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider
-      options={{
-        clientMaxAge: 60,  // Sync session state with server if it's older than 60 seconds
-        keepAlive: 5 * 60, // Send a keep alive request every 5 minutes
-      }}
       session={pageProps.session}
     >
       <Component {...pageProps} />
     </Provider>
   )
 }
+
