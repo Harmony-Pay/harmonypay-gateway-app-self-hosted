602252aea23981e8638d4f16b0e34a3d21a85cd3
Mauricio Zanetti Filho <contact@thinkincoin.com>
2023-06-26 12:31:13 -0300

    change host to 0.0.0.0

--- ecosystem.dev.config.js
+++ ecosystem.dev.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'harmonypay-gateway-app',
             script: './harmonypay-gateway-app/node_modules/next/dist/bin/next',
-            args: 'dev -H 10.128.0.3 -p 3033',
+            args: 'dev -H 0.0.0.0 -p 3033',
             cwd: './harmonypay-gateway-app',
         },
         {
