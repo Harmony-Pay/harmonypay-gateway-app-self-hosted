81626ef27b0ba6e53c7d0a469814c15f7ff05e9a
Mauricio Z. Filho <contact@thinkincoin.com>
2023-06-26 00:24:27 -0300

    Update [...nextauth].ts

--- harmonypay-gateway-app/pages/api/auth/[...nextauth].ts
+++ harmonypay-gateway-app/pages/api/auth/[...nextauth].ts
@@ -136,8 +136,8 @@ export default NextAuth({
   // https://next-auth.js.org/configuration/callbacks
   callbacks: {
     // async signIn({ user, account, profile, email, credentials }) { return true },
-    // async redirect({ url, baseUrl }),
-     async session({ session, token, user }),
+    // async redirect({ url, baseUrl }) { return baseUrl },
+     async session({ session, token, user }) { return session },
      async jwt({ token, user, account, profile, isNewUser }) { return token }
  
   },
